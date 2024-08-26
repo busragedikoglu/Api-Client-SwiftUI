dc5b843e5d45e4a8dceefa712dfbd61b78700ea8
busragedikoglu <bgedikogluu@gmail.com>
2024-08-26 20:40:17 +0300

    Initial Commit

---
+++ JokesAppSwiftUI/JokesAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokesAppSwiftUIApp.swift
+//  JokesAppSwiftUI
+//
+//  Created by Büşra Gedikoğlu on 26.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct JokesAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
